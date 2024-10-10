38a73b94cf6fc8b62f81ac6caf728add44a01f64
Victor Wade <p9859817888@gmail.com>
2024-10-10 07:50:09 -1000

    many typos

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,12 +1,12 @@
 const prisma = require("../prisma");
 const seed = async () =>
 	await prisma.employee.createMany({
-		data: [...new Array(10)].map(e, (idx) => ({ name: `Employee ${i}` })),
+		data: [...new Array(10)].map((e, idx) => ({ name: `Employee ${idx}` })),
 	});
 
 seed()
 	.then(async () => await prisma.$disconnect())
-	.catch(async (error) => {
+	.catch(async function (error) {
 		console.error(error);
 		await prisma.$disconnect();
 		process.exit(1);
